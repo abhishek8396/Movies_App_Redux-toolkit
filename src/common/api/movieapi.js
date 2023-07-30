7eeb62bc9e018357d35b778153b00fe477c7c70a
anhishek1289 <abhishekmedia1289@gmail.com>
2023-07-30 11:55:21 +0530

    newapi

--- src/common/api/movieapi.js
+++ src/common/api/movieapi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Fetch the base URL from environment variables
-const baseURL = process.env.NEXT_PUBLIC_OMDB_BASE_URL || "http://www.omdbapi.com";
+const baseURL = process.env.NEXT_PUBLIC_OMDB_BASE_URL || "https://www.omdbapi.com";
 
 // Create an Axios instance
 const axiosInstance = axios.create({
