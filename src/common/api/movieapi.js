fd32fb36e0b498eecfb7a09863766b84df6a18c7
anhishek1289 <abhishekmedia1289@gmail.com>
2023-07-30 11:49:33 +0530

    build

--- src/common/api/movieapi.js
+++ src/common/api/movieapi.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+// Fetch the base URL from environment variables
+const baseURL = process.env.NEXT_PUBLIC_OMDB_BASE_URL || "http://www.omdbapi.com";
 
-export default axios.create({
-    baseURL: "http://www.omdbapi.com",
-})
+// Create an Axios instance
+const axiosInstance = axios.create({
+  baseURL,
+});
+
+export default axiosInstance;
